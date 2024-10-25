7f218eda04f435e99c4b802a0c85d4b85e2ce3f3
Juan Carranza <juancarranza7561@gmail.com>
2024-10-24 23:22:05 -0500

    Add material (#11)

--- src/products/tasks.py
+++ src/products/tasks.py
@@ -220,6 +220,7 @@ def get_products(
                 length=db_product.length,
                 width=db_product.width,
                 height=db_product.height,
+                material=db_product.material,
                 status=db_product.product_status,
                 location_name=db_product.location_name,
                 purchase_price_mxn=db_product.purchase_price_mxn,
@@ -258,6 +259,7 @@ def get_product(product_id: int):
             length=db_product.length,
             width=db_product.width,
             height=db_product.height,
+            material=db_product.material,
             status=db_product.product_status,
             location_name=db_product.location_name,
             purchase_price_mxn=db_product.purchase_price_mxn,
@@ -417,6 +419,48 @@ def update_product_size(
             length=db_product.length,
             width=db_product.width,
             height=db_product.height,
+            material=db_product.material,
+            location_name=db_product.location_name,
+            purchase_price_mxn=db_product.purchase_price_mxn,
+            invoice_id=db_product.id_invoice,
+            invoice_status=db_product.invoice_status,
+            mx_iva=db_product.mx_iva,
+            profit=db_product.profit,
+            profit_percentage=db_product.profit_percentage,
+            sale_price=db_product.sale_price,
+            shipping_cost=db_product.shipping_cost,
+        ).dict()
+
+
+@shared_task
+def update_product_material(
+    product_id: int,
+    material: str,
+) -> schemas.ProductDetailResponse:
+    """Add product material to database by product_id"""
+    with Session() as session:
+        db_product = session.scalar(
+            select(models.Product)
+            .where(models.Product.id_product == product_id)
+        )
+        if not db_product:
+            raise ValueError(f"product with ID {product_id} not found.")
+        db_product.material = material
+        session.commit()
+        # Return the product using the query get_function_query
+        query = queries.get_product_query(session, product_id)
+        db_product = query.first()
+        return schemas.ProductDetailResponse(
+            id_product=db_product.id_product,
+            description=db_product.description,
+            shipping_label=db_product.shipping_label,
+            purchase_price=db_product.purchase_price,
+            shipping_group=db_product.shipping_group_name,
+            status=db_product.product_status,
+            length=db_product.length,
+            width=db_product.width,
+            height=db_product.height,
+            material=db_product.material,
             location_name=db_product.location_name,
             purchase_price_mxn=db_product.purchase_price_mxn,
             invoice_id=db_product.id_invoice,
@@ -521,6 +565,7 @@ def add_sale_price_with_id_2(
             length=db_product.length,
             width=db_product.width,
             height=db_product.height,
+            material=db_product.material,
             location_name=db_product.location_name,
             purchase_price_mxn=db_product.purchase_price_mxn,
             invoice_id=db_product.id_invoice,
