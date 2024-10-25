9aabc9806f4a4c469669de0d076c0254d898a17f
Juan Jose Carranza Garcia <juancarranza7561@gmail.com>
2024-10-24 22:45:08 -0500

    Include pylint

--- src/products/tasks.py
+++ src/products/tasks.py
@@ -313,7 +313,7 @@ def add_product_with_ids(
 
 
 @shared_task
-def update_product(
+def update_product(  # pylint: disable=too-many-arguments
     product_id: int,
     description: str | None = None,
     shipping_label: str | None = None,
